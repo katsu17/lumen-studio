5d7907e7fbe5111f0dfeccb2e37d752a6cb58c14
tandai katsuaki <enegoli17@gmail.com>
2019-08-20 15:17:48 +0900

    add pages portfolio, price, contact, privacyPolicy

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,16 @@ console.log("js successfully loaded!");
 var topBackGroundVideo = document.querySelectorAll('#topBackGroundVideo');
 topBackGroundVideo.autoplay  = true;
 topBackGroundVideo.loop  = true;
+
+<script type="text/javascript">
+$(function(){
+	$('a[href^=#]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	})
+});
+</script>
