d780515a50710849bc7d562ea9b27fe1d2b2584f
katsu <enegoli17@gmail.com>
2019-09-30 11:41:19 +0900

    create cursor action the spot right

--- public/js/script.js
+++ public/js/script.js
@@ -5,18 +5,6 @@ var topBackGroundVideo = document.querySelectorAll('#topBackGroundVideo');
 topBackGroundVideo.autoplay  = true;
 topBackGroundVideo.loop  = true;
 
-
-$(function(){
-	$('a[href^=#]').click(function(){
-		var speed = 500;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
-		return false;
-	})
-});
-
 function togglemenu() {
     var element = document.body.querySelector('.spMenus');
     element.classList.toggle("opened");
@@ -25,3 +13,4 @@ function togglemenu() {
     // var element = document.body.querySelector('.menuIcon');
     // element.classList.toggle("displayNone");
 }
+
