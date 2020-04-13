15786b14ef44bb853ff200beca5cf3d80a47a2e7
katsu17 <enegoli17@gmail.com>
2020-04-13 22:20:44 +0900

    ready to change postmail directry

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,40 @@ function togglemenu() {
     // element.classList.toggle("displayNone");
 }
 
+// input.onblur = function() {
+//     if (!input.value.includes('null')) { // not email
+//       input.classList.add('invalid');
+//       error.innerHTML = 'Please enter a correct email.'
+//     }
+//   };
+  
+// function checkForm(){
+//     var element = document.body.querySelector('.required');
+//     if(document.contactForm.name.value === "" || document.contactForm.name.value === null){
+//         element.classList.add(".borderRed");
+//     } else {
+//         element.classList.add(".borderBlue");
+//     };
+// };
+
+// document.querySelector(".required").style.border ="1px solid rgba(0, 166, 255, 7)";
+
+// function myfunc(value) {
+//  var add = document.querySelector(".required");
+
+// 	if(btn.style.display=="block"){
+// 		// noneで非表示
+// 		btn.style.display ="none";
+// 	}else{
+// 		// blockで表示
+// 		btn.style.display ="block";
+// 	}
+// }
+
+//   input.onfocus = function() {
+//     if (this.classList.contains('invalid')) {
+//       // ユーザは何かを再入力したいので "エラー" 表示を削除します
+//       this.classList.remove('invalid');
+//       error.innerHTML = "";
+//     }
+//   };
